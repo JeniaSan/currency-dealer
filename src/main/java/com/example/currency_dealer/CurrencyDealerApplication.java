7f5511338e1ae2a95574b81d4fd1c5c0c4880f50
Evhenii Polishuchenko <sds191sds@gmail.com>
2024-08-28 14:35:01 +0300

    init commit

---
+++ src/main/java/com/example/currency_dealer/CurrencyDealerApplication.java
@@ -0,0 +1,13 @@
+package com.example.currency_dealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyDealerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyDealerApplication.class, args);
+	}
+
+}
